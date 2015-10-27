04ecc58701d18c3b157ba98a7d5c90288af9e4ef
Jaehyun Lim <lim0606@gmail.com>
2015-10-27 08:03:41 -0500

    add a bash script to train VGG16_ilsvrc and a configuration file

---
+++ models/VGG16_ilsvrc15/train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+log_file_name='VGG16_ilsvrc15_train_'$(date +%Y%m%d_%H%M%S_%N)'.log'
+
+echo $log_file_name
+
+GLOG_logtostderr=1 python -u tools/train_net.py --gpu 1 --solver models/VGG16_ilsvrc15/solver.prototxt --weights data/imagenet_models/VGG16.v2.caffemodel --imdb ilsvrc_2015_train --cfg models/VGG16_ilsvrc15/config.yml --iters 2400000 2>&1 | tee $log_file_name
